cbd6c749121d97198b518741c41a792e6ad560bd
Aidar Gafiyatullin <ajdar.gafiat@gmail.com>
2025-02-19 21:14:56 +0300

    added homework 3

---
+++ home work 3/variables.tf
@@ -0,0 +1,13 @@
+variable "CLOUD_ID" {
+  type        = string
+  description = "Cloud ID"
+}
+
+variable "FOLDER_ID" {
+  type        = string
+  description = "Folder ID"
+}
+
+variable "ZONE" {
+  type = string
+}
---
+++ home work 3/vvot02.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  service_account_key_file = "/home/aidar/.yc-keys/key.json"
+  cloud_id                 = var.CLOUD_ID
+  folder_id                = var.FOLDER_ID
+  zone                     = var.ZONE
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "nextcloud-network"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "nextcloud-subnet"
+  zone           = var.ZONE
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  network_id     = yandex_vpc_network.network.id
+}
+
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2404-lts-oslogin"
+}
+
+resource "yandex_compute_disk" "boot-disk" {
+  name     = "nextcloud-boot-disk"
+  type     = "network-ssd"
+  image_id = data.yandex_compute_image.ubuntu.id
+  size     = 10
+}
+
+resource "yandex_compute_instance" "server" {
+  # TODO ADD ZONE
+  name        = "nextcloud-vm"
+  platform_id = "standard-v3"
+  hostname    = "nextcloud"
+
+  resources {
+    core_fraction = 50
+    cores         = 4
+    memory        = 4
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet.id
+    nat = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+output "web-server-ip" {
+  value = yandex_compute_instance.server.network_interface[0].nat_ip_address
+}
